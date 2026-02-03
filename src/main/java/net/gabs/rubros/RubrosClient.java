8b6eb8f625ca544b0831464c2f80e11d55614cd8
gabs <eiudoglb@gmail.com>
2026-02-03 14:30:06 -0300

    Initial Commit

---
+++ src/main/java/net/gabs/rubros/RubrosClient.java
@@ -0,0 +1,10 @@
+package net.gabs.rubros;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RubrosClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
